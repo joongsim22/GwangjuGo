cbd1c961491bcca7d5580e77697d87a5b759dfff
dawnferre <ayj1002@naver.com>
2017-04-01 17:34:25 +0900

    다시 올림

---
+++ app/src/main/java/gwangju/com/list/Constant.java
@@ -0,0 +1,10 @@
+package gwangju.com.list;
+
+/**
+ * Created by user on 2017-04-01.
+ */
+
+public class Constant {
+    //	public static final String SERVER_URL = "http://168.131.22.107:8080/JSP";
+    public static final String SERVER_URL = "http://ayj1002.cafe24.com";
+}
---
+++ app/src/main/java/gwangju/com/list/JavaRoomsDto.java
@@ -0,0 +1,68 @@
+package gwangju.com.list;
+
+import java.io.Serializable;
+
+/**
+ * Created by user on 2017-04-01.
+ */
+
+public class JavaRoomsDto implements Serializable {
+    int num;
+    String kind;
+    String name;
+    String location;
+    String phoneNum;
+    int feeMin;
+    int feeMax;
+
+    public int getNum() {
+        return num;
+    }
+    public void setNum(int num) {
+        this.num = num;
+    }
+    public String getKind() {
+        return kind;
+    }
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+    public int getFeeMin() {
+        return feeMin;
+    }
+    public void setFeeMin(int feeMin) {
+        this.feeMin = feeMin;
+    }
+    public int getFeeMax() {
+        return feeMax;
+    }
+    public void setFeeMax(int feeMax) {
+        this.feeMax = feeMax;
+    }
+
+
+    @Override
+    public String toString() {
+        return "ItemVO [name=" + name +", location=" + location +", kind=" + kind
+                + ", phoneNum=" + phoneNum + ", feeMin=" + feeMin +", feeMax=" + feeMax + "]";
+    }
+
+}
